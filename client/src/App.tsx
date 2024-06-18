082783e746fd479f59ef97b657213aa823b44a58
Henry Burgess <henryjburg@gmail.com>
2024-06-18 15:49:59 -0500

    Refactor and begin creating custom client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, Flex, FormControl, Heading, Input, Text } from '@chakra-ui/react'
+import { CheckCircleIcon, RepeatIcon, WarningIcon } from '@chakra-ui/icons'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const validIP = (ip: string): boolean => {
+  // https://stackoverflow.com/a/27434991
+  return /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ip);
+}
+
+const App = () => {
+  // Input states
+  const [invalidInput, setInvalidInput] = useState(false);
+
+  // Connectivity and status update states
+  const [connected, setConnected] = useState(false);
+  const [lastScreenshot, setLastScreenshot] = useState("Never");
+  const [lastStatus, setLastStatus] = useState("Never");
+  const [address, setAddress] = useState("192.168.0.1")
+
+  // Response states
+  const [elapsedTime, setElapsedTime] = useState(0.0);
+  const [activeBlock, setActiveBlock] = useState("Inactive");
+
+  const updateAddress = (event: any) => {
+    setInvalidInput(!validIP(event.target.value));
+    setAddress(event.target.value);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Flex w={"100%"} minH={"100vh"} direction={"column"} gap={"4"} p={"4"}>
+      <Heading>Headsup :: Headset</Heading>
+      <Flex w={"60%"}>
+        <FormControl isInvalid={invalidInput}>
+          <Flex w={"100%"} direction={"row"} gap={"2"}>
+            <Flex w={"60%"}>
+              <Input placeholder={"Headset Local IP Address"} value={address} onChange={updateAddress} />
+            </Flex>
+            <Button colorScheme={"green"} leftIcon={<RepeatIcon />} isDisabled={invalidInput}>
+              Test Connectivity
+            </Button>
+            <Flex direction={"row"} align={"center"} gap={"2"}>
+              {connected ? <CheckCircleIcon color={"green"} /> : <WarningIcon color={"red"} />}
+              <Text color={connected ? "green.600" : "red.600"} fontWeight={"semibold"}>{connected ? "Connected!" : "Not connected"}</Text>
+            </Flex>
+          </Flex>
+        </FormControl>
+      </Flex>
+      <Flex direction={"row"} gap={"2"}>
+        <Flex w={"70%"} direction={"column"} gap={"2"}>
+          <Heading size={"md"}>Headset View</Heading>
+          <Flex w={"100%"} direction={"row"} gap={"6"} align={"center"}>
+            <Button colorScheme={"blue"} isDisabled={!connected}>Refresh</Button>
+            <Text color={"gray.500"} fontSize={"sm"}>Last Update: {lastScreenshot}</Text>
+          </Flex>
+          <Flex><Text>Screenshot Placeholder</Text></Flex>
+        </Flex>
+        <Flex w={"30%"} direction={"column"} gap={"2"}>
+          <Heading size={"md"}>Headset Status</Heading>
+          <Flex w={"100%"} direction={"row"} gap={"6"} align={"center"}>
+            <Button colorScheme={"blue"} isDisabled={!connected}>Refresh</Button>
+            <Text color={"gray.500"} fontSize={"sm"}>Last Update: {lastStatus}</Text>
+          </Flex>
+          <Heading size={"sm"}>Status</Heading>
+          <Text>Active block: {activeBlock}</Text>
+          <Text>Elapsed time: {elapsedTime}</Text>
+        </Flex>
+      </Flex>
+    </Flex>
   )
 }
 
--- server/README.md
+++
@@ -1,3 +0,0 @@
-# Headsup - server
-
-Server application based on the example web application for [UnityRenderStreaming](https://github.com/Unity-Technologies/UnityRenderStreaming).
